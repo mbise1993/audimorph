316a49d5adbfd7cf1b7a005fc12c276ddb07a295
Matt Bise <mbise1993@gmail.com>
2020-10-29 01:28:36 -0500

    Setup Amplify API

---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,39 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateTemplate = /* GraphQL */ `
+  subscription OnCreateTemplate($owner: String) {
+    onCreateTemplate(owner: $owner) {
+      id
+      name
+      description
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const onUpdateTemplate = /* GraphQL */ `
+  subscription OnUpdateTemplate($owner: String) {
+    onUpdateTemplate(owner: $owner) {
+      id
+      name
+      description
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const onDeleteTemplate = /* GraphQL */ `
+  subscription OnDeleteTemplate($owner: String) {
+    onDeleteTemplate(owner: $owner) {
+      id
+      name
+      description
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
