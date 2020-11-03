54e288450ab4e2289d7e576cdb11fbfc8486be17
Matt Bise <mbise1993@gmail.com>
2020-11-03 03:23:39 -0600

    Listen for auth events

--- client/state/authState.js
+++ client/state/authState.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import { Auth, Hub } from 'aws-amplify';
 import { createContainer } from 'unstated-next';
 
 function useAuth() {
   const [currentUser, setCurrentUser] = React.useState();
+
+  React.useEffect(() => {
+    const authListener = (data) => {
+      if (data.payload.event === 'signIn') {
+        Auth.currentAuthenticatedUser().then((user) => {
+          setCurrentUser(user);
+        });
+      }
+    };
+
+    Hub.listen('auth', authListener);
+
+    return () => {
+      Hub.remove('auth', authListener);
+    };
+  }, []);
 
   return {
     currentUser,
